724ff921193ca3186bec660d6ac99163beeee2d3
안찬웅 <1stevering@naver.com>
2024-11-14 12:13:08 +0900

    [feat/#32]: CardService 작업

--- src/main/java/com/wekids/backend/card/service/CardService.java
+++ src/main/java/com/wekids/backend/card/service/CardService.java
@@ -1,4 +1,9 @@
 package com.wekids.backend.card.service;
 
+import com.wekids.backend.card.domain.Card;
+
+import java.util.Optional;
+
 public interface CardService {
+    Optional<Card> findById(Long cardId);
 }
