cbe204ac1597a9370e27640eaa7796fe7cfbf2ca
안찬웅 <1stevering@naver.com>
2024-11-14 12:18:53 +0900

    [feat/#32]: 카드 id 값 넣으면 카드 조회

--- src/main/java/com/wekids/backend/card/service/CardServiceImpl.java
+++ src/main/java/com/wekids/backend/card/service/CardServiceImpl.java
@@ -1,14 +1,24 @@
 package com.wekids.backend.card.service;
 
-import com.wekids.backend.account.service.AccountService;
+import com.wekids.backend.card.domain.Card;
+import com.wekids.backend.card.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Slf4j
-public class CardServiceImpl implements AccountService {
+public class CardServiceImpl implements CardService {
+
+    private final CardRepository cardRepository;
+
+    @Override
+    public Optional<Card> findById(Long cardId) {
+        return cardRepository.findById(cardId);
+    }
 }
