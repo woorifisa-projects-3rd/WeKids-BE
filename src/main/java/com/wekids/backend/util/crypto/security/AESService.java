e2d5c880c48326894304ff337dd59f0b5a24072d
Kang Hyun Woo <gusdn741021@gmail.com>
2024-12-21 20:38:09 +0900

    [refact/#90]: 개행 넣어서 가독성 높이기

--- src/main/java/com/wekids/backend/util/crypto/security/AESService.java
+++ src/main/java/com/wekids/backend/util/crypto/security/AESService.java
@@ -29,15 +29,20 @@ public class AESService implements PrivacyEncryptor {
     public String encrypt(String plainText) throws Exception {
         Cipher cipher = Cipher.getInstance(AES_GCM_NO_PADDING);
         SecretKey secretKey = new SecretKeySpec(this.secretKey.getBytes(), AES);
+
         byte[] iv = new byte[GCM_IV_LENGTH];
         SecureRandom secureRandom = new SecureRandom();
         secureRandom.nextBytes(iv);
+
         GCMParameterSpec parameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, iv);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
+
         byte[] cipherText = cipher.doFinal(plainText.getBytes());
+
         byte[] encrypted = new byte[iv.length + cipherText.length];
         System.arraycopy(iv, 0, encrypted, 0, iv.length);
         System.arraycopy(cipherText, 0, encrypted, iv.length, cipherText.length);
+
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
@@ -45,13 +50,17 @@ public class AESService implements PrivacyEncryptor {
     public String decrypt(String encryptedText) throws Exception {
         Cipher cipher = Cipher.getInstance(AES_GCM_NO_PADDING);
         SecretKey secretKey = new SecretKeySpec(this.secretKey.getBytes(), AES);
+
         byte[] decoded = Base64.getDecoder().decode(encryptedText);
         byte[] iv = Arrays.copyOfRange(decoded, 0, GCM_IV_LENGTH);
         byte[] cipherText = Arrays.copyOfRange(decoded, GCM_IV_LENGTH, decoded.length);
+
         GCMParameterSpec parameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, iv);
         cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+
         byte[] plainText = cipher.doFinal(cipherText);
         return new String(plainText);
     }
 
 }
+
